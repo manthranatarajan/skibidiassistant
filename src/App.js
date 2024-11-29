c5ffa111f4877347968ce35aa9677ad08dd77422
Temiloluwa <97484828+Temii-mp3@users.noreply.github.com>
2024-11-29 13:11:22 -0600

    SKIBIDI TOILET OHIO SIGMA FROMT HE SCREEN TO THE RING TO THE BEND TO THE KING WHERES MY CROWN

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className='mainContainer'>
+    <div className="app">
+      <div className="video-container">
+        <video controls autoPlay loop muted width="200" height="600">
+          <source src="/vibeos/vibeo1.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className='parentContainer'>
+        <input type='text' id='engtxt' placeholder='Englis text goes here....'></input>
+        <button id='translate'>
+          translate
+        </button>
+        <div id='brottxt'><p>Skibidi toilet sigma text</p></div>
+      </div>
+      <div className="video-container">
+        <video controls autoPlay loop muted width="200" height="600">
+          <source src="/vibeos/vibeo2.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
+    <h1 id='title'>BRAINOT TRANSLATOR
+    <p id='subhead'>Skibbidi sigma ohio gyat ermm what da sigma knee surgery tomorrow fanum tax from the screen to the ring to the bend to the king</p>
+    </h1>
+
+  </div>
+</div>
   );
 }
 
